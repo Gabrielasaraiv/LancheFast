3f2c0e1716a85ce76740e78709f3131a131ba37b
Gabrielasaraiv <gabrielasaraivaaraujo358@gmail.com>
2025-04-10 13:03:08 -0300

    implementado model, migration, index e seeder do crud de produto

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use App\Livewire\Clientes\Create;
 use App\Livewire\Clientes\Edit;
 use App\Livewire\Clientes\Index;
 use App\Livewire\Clientes\Show;
+use App\Livewire\Produtos\Create as ProdutosCreate;
+use App\Livewire\Produtos\Edit as ProdutosEdit;
+use App\Livewire\Produtos\Index as ProdutosIndex;
+use App\Livewire\Produtos\Show as ProdutosShow;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('clientes')->group(function () {
@@ -11,4 +15,11 @@ Route::prefix('clientes')->group(function () {
     Route::get('/create', Create::class)->name('clientes.create');
     Route::get('/{id}', Show::class)->name('clientes.show');
     Route::get('/{id}/edit', Edit::class)->name('clientes.edit');
+});
+
+Route::prefix('produtos')->group(function () {
+    Route::get('/', ProdutosIndex::class)->name('produtos.index');
+    Route::get('/create', ProdutosCreate::class)->name('produtos.create');
+    Route::get('/{produto}', ProdutosShow::class)->name('produtos.show');
+    Route::get('/{produto}/edit', ProdutosEdit::class)->name('produtos.edit');
 });
